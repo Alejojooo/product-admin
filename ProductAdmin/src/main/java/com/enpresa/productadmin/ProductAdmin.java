52ab3aac5f009a01a1a096e2bd72af58af7e4619
Alejojooo <alejandrofn2204@gmail.com>
2023-10-22 14:49:20 -0600

    Proyecto nuevo con Maven

---
+++ ProductAdmin/src/main/java/com/enpresa/productadmin/ProductAdmin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.enpresa.productadmin;
+
+/**
+ *
+ * @author Alejo
+ */
+public class ProductAdmin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
