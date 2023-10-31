17a824e0302b04615d902822a9800907ed947616
Alejojooo <alejandrofn2204@gmail.com>
2023-10-31 14:30:23 -0600

    Update ProductoDAO.java

--- ProductAdmin/src/main/java/com/enpresa/productadmin/dao/ProductoDAO.java
+++ ProductAdmin/src/main/java/com/enpresa/productadmin/dao/ProductoDAO.java
@@ -4,7 +4,11 @@ import com.enpresa.productadmin.modelo.Producto;
 import com.enpresa.productadmin.utils.Conexion;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -30,12 +34,61 @@ public class ProductoDAO {
         }
     }
 
-    public void modificar() {
+    public void modificar(Producto producto) {
+        String sql = "{CALL dbo.pModificarProducto(?, ?, ?, ?, ?, ?)}";
 
+        try (Connection conexion = new Conexion().establecerConexion(); CallableStatement cs = conexion.prepareCall(sql)) {
+
+            cs.setInt(1, producto.getId());
+            cs.setString(2, producto.getNombre());
+            cs.setInt(3, producto.getCantidad());
+            cs.setBigDecimal(4, producto.getPrecioCompra());
+            cs.setBigDecimal(5, producto.getPrecioVenta());
+            cs.setString(6, producto.getDescripcion());
+
+            cs.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void eliminar() {
+    public void eliminar(Producto producto) {
+        String sql = "{CALL dbo.pEliminarProducto(?)}";
 
+        try (Connection conexion = new Conexion().establecerConexion(); CallableStatement cs = conexion.prepareCall(sql)) {
+
+            cs.setInt(1, producto.getId());
+
+            cs.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Producto> consultarTodos() {
+        List<Producto> productos = new ArrayList<>();
+        String sql = "SELECT * FROM vProductos";
+
+        try (Connection conexion = new Conexion().establecerConexion(); Statement st = conexion.createStatement()) {
+            ResultSet rs = st.executeQuery(sql);
+            Producto producto;
+            while (rs.next()) {
+                producto = new Producto();
+                producto.setId(rs.getInt(1));
+                producto.setNombre(rs.getString(2));
+                producto.setCantidad(rs.getInt(3));
+                producto.setPrecioCompra(rs.getBigDecimal(4));
+                producto.setPrecioVenta(rs.getBigDecimal(5));
+                producto.setDescripcion(rs.getString(6));
+                productos.add(producto);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return productos;
     }
 
     public void buscar() {
