c5d851facb34311d605aa65ba33265ca8d9ca669
jmduro <jmdubonx@gmail.com>
2023-10-31 10:52:10 -0600

    Update Conexion.java

--- ProductAdmin/src/main/java/com/enpresa/productadmin/utils/Conexion.java
+++ ProductAdmin/src/main/java/com/enpresa/productadmin/utils/Conexion.java
@@ -1,9 +1,43 @@
 package com.enpresa.productadmin.utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author Alejo
  */
 public class Conexion {
-    
+    //Conectarse a la base de datos
+    //Remplazar nombre servidor, usuario, y contraseña.
+    public static void main(String[] args) {
+        String connectionUrl =
+                "jdbc:sqlserver://yourserver.database.windows.net:1433;"
+                        + "database=AdventureWorks;"
+                        + "user=yourusername@yourserver;"
+                        + "password=yourpassword;"
+                        + "encrypt=true;"
+                        + "trustServerCertificate=false;"
+                        + "loginTimeout=30;";
+
+        try (Connection connection = DriverManager.getConnection(connectionUrl);
+                Statement statement = connection.createStatement();) {
+            // Code here.
+            /*
+            //
+            String selectSql = "SELECT * FROM tabla";
+            resultSet = statement.executeQuery(selectSql);
+            
+            while(result.next()){
+                //Esto no lo entendí
+                System.out.println(result.getString(2) + " " + resultSet.getString(3));
+            } */
+        }
+        // Handle any errors that may have occurred.
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
