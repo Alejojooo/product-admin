6e052f7a4d88a2c0280a6febe1069bbbce838b8d
jmduro <jmdubonx@gmail.com>
2023-10-31 15:55:29 -0600

    Update UsuarioDAO.java

--- ProductAdmin/src/main/java/com/enpresa/productadmin/dao/UsuarioDAO.java
+++ ProductAdmin/src/main/java/com/enpresa/productadmin/dao/UsuarioDAO.java
@@ -22,21 +22,48 @@ public class UsuarioDAO {
             cs.setString(4, usuario.getApellidos());
             cs.setString(5, usuario.getRol().toString());
             
+            cs.execute();
+            
         } catch (SQLException e) {
             e.printStackTrace();
         }
         
     }
 
-    public void modificar() {
-
+    public void modificar(Usuario usuario) {
+        String sql = "{CALL dbo.pModificarUsuario(?, ?, ?, ?, ?)}";
+        
+        try (Connection conexion = new Conexion().establecerConexion(); CallableStatement cs = conexion.prepareCall(sql)) {
+            
+            cs.setInt(1, usuario.getId());
+            cs.setString(2, usuario.getUsuario());
+            cs.setString(3, usuario.getNombres());
+            cs.setString(4, usuario.getApellidos());
+            cs.setString(5, usuario.getRol().toString());
+            
+            cs.execute();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
     }
 
-    public void eliminar() {
-
+    public void eliminar(Usuario usuario) {
+        String sql = "{CALL dbo.pEliminarUsuario(?)}";
+        
+        try (Connection conexion = new Conexion().establecerConexion(); CallableStatement cs = conexion.prepareCall(sql)) {
+            
+            cs.setInt(1, usuario.getId());
+            
+            cs.execute();
+            
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void buscar() {
-
+        
     }
 }
