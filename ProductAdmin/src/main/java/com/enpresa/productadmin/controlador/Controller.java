62e745bffa905be21f97f2c1e4fda438848db7ea
Alejojooo <alejandrofn2204@gmail.com>
2023-11-01 12:57:23 -0600

    Create Controller.java

---
+++ ProductAdmin/src/main/java/com/enpresa/productadmin/controlador/Controller.java
@@ -0,0 +1,29 @@
+package com.enpresa.productadmin.controlador;
+
+import com.enpresa.productadmin.dao.DAO;
+import com.enpresa.productadmin.dao.UsuarioDAO;
+import com.enpresa.productadmin.vistas.IniciarSesion;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Alejo
+ */
+public abstract class Controller {
+
+    private final DAO modelo;
+    private final JPanel vista;
+    private final JFrame frame;
+
+    public Controller(DAO modelo, JPanel vista) {
+        this.modelo = modelo;
+        this.vista = vista;
+
+        frame = new JFrame();
+        frame.setContentPane(vista);
+        frame.pack();
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+}
