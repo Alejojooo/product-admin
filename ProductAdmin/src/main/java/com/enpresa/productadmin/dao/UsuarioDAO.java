cb30d61303d9b77f5fbfd14d425759ad94d039c3
Alejojooo <alejandrofn2204@gmail.com>
2023-11-01 12:58:43 -0600

    Métodos adicionales para login

--- ProductAdmin/src/main/java/com/enpresa/productadmin/dao/UsuarioDAO.java
+++ ProductAdmin/src/main/java/com/enpresa/productadmin/dao/UsuarioDAO.java
@@ -5,6 +5,7 @@ import com.enpresa.productadmin.modelo.Usuario;
 import com.enpresa.productadmin.utils.Conexion;
 import java.sql.CallableStatement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,69 +17,85 @@ import java.util.List;
  * @author Alejo
  */
 public class UsuarioDAO implements DAO<Usuario> {
-
+    
     @Override
     public void crear(Usuario usuario) {
         String sql = "{CALL dbo.pCrearUsuario(?, ?, ?, ?, ?)}";
-
-        try (Connection conexion = new Conexion().establecerConexion(); CallableStatement cs = conexion.prepareCall(sql)) {
-
+        
+        try (Connection c = new Conexion().establecerConexion(); CallableStatement cs = c.prepareCall(sql)) {
+            
             cs.setString(1, usuario.getUsuario());
-            cs.setString(2, usuario.getPassword());
+            cs.setString(2, usuario.getClave());
             cs.setString(3, usuario.getNombres());
             cs.setString(4, usuario.getApellidos());
             cs.setString(5, usuario.getRol().toString());
-
+            
             cs.execute();
-
+            
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
     }
-
+    
     @Override
     public void modificar(Usuario usuario) {
         String sql = "{CALL dbo.pModificarUsuario(?, ?, ?, ?, ?)}";
-
-        try (Connection conexion = new Conexion().establecerConexion(); CallableStatement cs = conexion.prepareCall(sql)) {
-
+        
+        try (Connection c = new Conexion().establecerConexion(); CallableStatement cs = c.prepareCall(sql)) {
+            
             cs.setInt(1, usuario.getId());
             cs.setString(2, usuario.getUsuario());
             cs.setString(3, usuario.getNombres());
             cs.setString(4, usuario.getApellidos());
             cs.setString(5, usuario.getRol().toString());
-
+            
             cs.execute();
-
+            
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
     }
-
+    
     @Override
     public void eliminar(Usuario usuario) {
         String sql = "{CALL dbo.pEliminarUsuario(?)}";
-
-        try (Connection conexion = new Conexion().establecerConexion(); CallableStatement cs = conexion.prepareCall(sql)) {
-
+        
+        try (Connection c = new Conexion().establecerConexion(); CallableStatement cs = c.prepareCall(sql)) {
             cs.setInt(1, usuario.getId());
-
             cs.execute();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
-
+    
+    public Usuario consultarUno(int id) {
+        Usuario usuario = new Usuario();
+        String sql = "{CALL dbo.pConsultarUsuario(?)}";
+        
+        try (Connection c = new Conexion().establecerConexion(); CallableStatement cs = c.prepareCall(sql)) {
+            cs.setInt(1, id);
+            ResultSet rs = cs.executeQuery();
+            if (rs.next()) {
+                usuario.setId(rs.getInt(1));
+                usuario.setUsuario(rs.getString(2));
+                usuario.setNombres(rs.getString(3));
+                usuario.setApellidos(rs.getString(4));
+                usuario.setRol(Rol.valueOf(rs.getString(5)));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuario;
+    }
+    
     @Override
     public List<Usuario> consultarTodos() {
         List<Usuario> usuarios = new ArrayList<>();
         String sql = "SELECT * FROM vUsuarios";
-
-        try (Connection conexion = new Conexion().establecerConexion(); Statement st = conexion.createStatement()) {
-            ResultSet rs = st.executeQuery(sql);
+        
+        try (Connection c = new Conexion().establecerConexion(); Statement st = c.createStatement(); ResultSet rs = st.executeQuery(sql)) {
             Usuario usuario;
             while (rs.next()) {
                 usuario = new Usuario();
@@ -92,11 +109,27 @@ public class UsuarioDAO implements DAO<Usuario> {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-
+        
         return usuarios;
     }
-
+    
+    public int login(String usuario, String clave) {
+        String sql = "SELECT dbo.fLogin(?, ?)";
+        
+        try (Connection c = new Conexion().establecerConexion(); PreparedStatement ps = c.prepareCall(sql)) {
+            ps.setString(1, usuario);
+            ps.setString(2, clave);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                return rs.getInt(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+    
     public void buscar() {
-
+        
     }
 }
