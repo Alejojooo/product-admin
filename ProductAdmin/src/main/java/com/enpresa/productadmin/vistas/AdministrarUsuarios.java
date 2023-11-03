1f32ba9e625c2fd534e13fb9e568c0d45a64284f
Alejojooo <alejandrofn2204@gmail.com>
2023-11-02 21:36:48 -0600

    Fix AdministrarUsuarios

--- ProductAdmin/src/main/java/com/enpresa/productadmin/vistas/AdministrarUsuarios.java
+++ ProductAdmin/src/main/java/com/enpresa/productadmin/vistas/AdministrarUsuarios.java
@@ -16,7 +16,7 @@ public class AdministrarUsuarios extends javax.swing.JPanel {
     public AdministrarUsuarios() {
         initComponents();
     }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,26 +27,26 @@ public class AdministrarUsuarios extends javax.swing.JPanel {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        tbAdminUsuarios = new javax.swing.JTable();
+        tbUsuarios = new javax.swing.JTable();
         jPanel1 = new javax.swing.JPanel();
         btnAgregar = new javax.swing.JButton();
-        btnEditar = new javax.swing.JButton();
+        btnModificar = new javax.swing.JButton();
         btnEliminar = new javax.swing.JButton();
         btnBuscar = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         txtUsuario = new javax.swing.JTextField();
-        txtApellido = new javax.swing.JTextField();
+        txtApellidos = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
-        txtNombre = new javax.swing.JTextField();
+        txtNombres = new javax.swing.JTextField();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         cBoxRol = new javax.swing.JComboBox<>();
-        txtID = new javax.swing.JTextField();
+        txtId = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
 
         setLayout(null);
 
-        tbAdminUsuarios.setModel(new javax.swing.table.DefaultTableModel(
+        tbUsuarios.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -54,7 +54,7 @@ public class AdministrarUsuarios extends javax.swing.JPanel {
 
             }
         ));
-        jScrollPane1.setViewportView(tbAdminUsuarios);
+        jScrollPane1.setViewportView(tbUsuarios);
 
         add(jScrollPane1);
         jScrollPane1.setBounds(16, 41, 578, 474);
@@ -67,10 +67,10 @@ public class AdministrarUsuarios extends javax.swing.JPanel {
         jPanel1.add(btnAgregar);
         btnAgregar.setBounds(44, 23, 125, 50);
 
-        btnEditar.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        btnEditar.setText("Editar");
-        jPanel1.add(btnEditar);
-        btnEditar.setBounds(185, 23, 125, 50);
+        btnModificar.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        btnModificar.setText("Modificar");
+        jPanel1.add(btnModificar);
+        btnModificar.setBounds(185, 23, 125, 50);
 
         btnEliminar.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         btnEliminar.setText("Eliminar");
@@ -90,17 +90,17 @@ public class AdministrarUsuarios extends javax.swing.JPanel {
         jPanel1.add(txtUsuario);
         txtUsuario.setBounds(77, 240, 200, 27);
 
-        txtApellido.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jPanel1.add(txtApellido);
-        txtApellido.setBounds(77, 368, 200, 27);
+        txtApellidos.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jPanel1.add(txtApellidos);
+        txtApellidos.setBounds(77, 368, 200, 27);
 
         jLabel2.setText("Apellidos:");
         jPanel1.add(jLabel2);
         jLabel2.setBounds(77, 347, 56, 21);
 
-        txtNombre.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jPanel1.add(txtNombre);
-        txtNombre.setBounds(77, 304, 200, 27);
+        txtNombres.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jPanel1.add(txtNombres);
+        txtNombres.setBounds(77, 304, 200, 27);
 
         jLabel3.setText("Nombres:");
         jPanel1.add(jLabel3);
@@ -115,9 +115,9 @@ public class AdministrarUsuarios extends javax.swing.JPanel {
         jPanel1.add(cBoxRol);
         cBoxRol.setBounds(77, 431, 200, 27);
 
-        txtID.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
-        jPanel1.add(txtID);
-        txtID.setBounds(77, 176, 200, 27);
+        txtId.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+        jPanel1.add(txtId);
+        txtId.setBounds(77, 176, 200, 27);
 
         jLabel5.setText("ID:");
         jPanel1.add(jLabel5);
@@ -131,8 +131,8 @@ public class AdministrarUsuarios extends javax.swing.JPanel {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnAgregar;
     private javax.swing.JButton btnBuscar;
-    private javax.swing.JButton btnEditar;
     private javax.swing.JButton btnEliminar;
+    private javax.swing.JButton btnModificar;
     private javax.swing.JComboBox<String> cBoxRol;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
@@ -141,10 +141,10 @@ public class AdministrarUsuarios extends javax.swing.JPanel {
     private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable tbAdminUsuarios;
-    private javax.swing.JTextField txtApellido;
-    private javax.swing.JTextField txtID;
-    private javax.swing.JTextField txtNombre;
+    private javax.swing.JTable tbUsuarios;
+    private javax.swing.JTextField txtApellidos;
+    private javax.swing.JTextField txtId;
+    private javax.swing.JTextField txtNombres;
     private javax.swing.JTextField txtUsuario;
     // End of variables declaration//GEN-END:variables
 }
